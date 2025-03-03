0b042d02ebeaaae1c3e0d4518d82e73408755fff
Aljhon_Hangad <aljhonh9@gmail.com>
2025-03-03 15:39:27 +0800

    Add files via upload

---
+++ Chapter 1/SimpleFormsApp.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SimpleFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fname = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(86, 101);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(100, 20);
+            this.fname.TabIndex = 0;
+            this.fname.TextChanged += new System.EventHandler(this.fname_TextChanged);
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(86, 156);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(100, 20);
+            this.lname.TabIndex = 1;
+            this.lname.TextChanged += new System.EventHandler(this.lname_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(86, 207);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 4;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 307);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.fname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Submit;
+    }
+}
+
---
+++ Chapter 1/SimpleFormsApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            string FirstNameValue = fname.Text;
+            string LastNameValue = lname.Text;
+            MessageBox.Show($"Hello There! {FirstNameValue} {LastNameValue}","Greetings!", MessageBoxButtons.OK , MessageBoxIcon.Information);
+        }
+    }
+}
