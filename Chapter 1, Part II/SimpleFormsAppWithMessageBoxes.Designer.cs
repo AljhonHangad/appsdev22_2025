d1500865c15396e75ea8f1990e6aa4cb068b5c4d
Aljhon_Hangad <aljhonh9@gmail.com>
2025-03-05 10:27:27 +0800

    Add files via upload

---
+++ Chapter 1, Part II/SimpleFormsAppWithMessageBoxes.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.mname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.suffix = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Middle Name";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(110, 97);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(133, 20);
+            this.fname.TabIndex = 2;
+            this.fname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // mname
+            // 
+            this.mname.Location = new System.Drawing.Point(110, 165);
+            this.mname.Name = "mname";
+            this.mname.Size = new System.Drawing.Size(133, 20);
+            this.mname.TabIndex = 3;
+            this.mname.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // lname
+            // 
+            this.lname.Location = new System.Drawing.Point(110, 228);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(133, 20);
+            this.lname.TabIndex = 5;
+            this.lname.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Suffix";
+            // 
+            // suffix
+            // 
+            this.suffix.Location = new System.Drawing.Point(110, 291);
+            this.suffix.Name = "suffix";
+            this.suffix.Size = new System.Drawing.Size(133, 20);
+            this.suffix.TabIndex = 7;
+            this.suffix.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 436);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.suffix);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.mname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox mname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox suffix;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Chapter 1, Part II/SimpleFormsAppWithMessageBoxes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstname = fname.Text;
+            string middlename = mname.Text;
+            string lastname = lname.Text;
+            string suffixValue = suffix.Text;
+
+            
+            DialogResult result = MessageBox.Show("Are you sure you want to proceed?", "Confirmation",MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show($"Hello There! {firstname} {middlename} {lastname} {suffixValue}", "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show($"You clicked No, action canceled.", "Exit", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+        }
+    }
+}
