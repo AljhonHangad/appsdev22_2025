bc0264629d4325cc1ecd527391c873196339abf3
Aljhon_Hangad <aljhonh9@gmail.com>
2025-03-10 10:24:11 +0800

    Add files via upload

---
+++ Chapter 1 Working with ListBox - Part I/SimpleFormsAppWithListBox.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace SimpleFormsAppWithListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(24, 108);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(299, 277);
+            this.lstBox.TabIndex = 0;
+            // 
+            // addbutton
+            // 
+            this.addbutton.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addbutton.Location = new System.Drawing.Point(155, 79);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(51, 25);
+            this.addbutton.TabIndex = 2;
+            this.addbutton.Text = "Add";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.Location = new System.Drawing.Point(212, 79);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(52, 24);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(270, 79);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(53, 24);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(24, 81);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(125, 20);
+            this.txtBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label1.Location = new System.Drawing.Point(44, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SIMPLE NAME VIEWER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(349, 430);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.lstBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
