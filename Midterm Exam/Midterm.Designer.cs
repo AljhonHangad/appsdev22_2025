fff6aa10ad5bb29e3cdbcef1a825f75b8cc751b9
Aljhon_Hangad <aljhonh9@gmail.com>
2025-03-19 11:15:35 +0800

    Add files via upload

---
+++ Midterm Exam/Midterm.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.compute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(33, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(162, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(219, 74);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(155, 169);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(416, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // compute
+            // 
+            this.compute.Location = new System.Drawing.Point(416, 110);
+            this.compute.Name = "compute";
+            this.compute.Size = new System.Drawing.Size(121, 23);
+            this.compute.TabIndex = 4;
+            this.compute.Text = "Compute";
+            this.compute.UseVisualStyleBackColor = true;
+            this.compute.Click += new System.EventHandler(this.compute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Iphone Model:";
+            this.label1.Click += new System.EventHandler(this.compute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Add-Ons:";
+            // 
+            // loan
+            // 
+            this.loan.AutoSize = true;
+            this.loan.Location = new System.Drawing.Point(413, 47);
+            this.loan.Name = "loan";
+            this.loan.Size = new System.Drawing.Size(66, 13);
+            this.loan.TabIndex = 7;
+            this.loan.Text = "Loan Terms:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 512);
+            this.Controls.Add(this.loan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.compute);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button compute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label loan;
+    }
+}
+
---
+++ Midterm Exam/Midterm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] phones = { "Iphone 16 Pro Max (255GB)", "Iphone 16 Pro (128GB)", "Iphone 16 Plus (128GB)", "Iphone 16 (128B)", "Iphone 16e (128B)" };
+            listBox1.Items.AddRange(phones);
+
+            string[] addOn = { "Apple Care Plus", "USB-C Charging Adapter", "MagSafe Charger (1M)" };
+            checkedListBox1.Items.AddRange(addOn);
+
+            string[] loan = { "12 Months", "24 Months", "30 Months", "36 Months" };
+            comboBox1.Items.AddRange(loan);
+
+            listBox1.SelectedItem = 0;
+            checkedListBox1.SelectedItem = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Contains(listBox1.Text);
+            if (listBox1.SelectedIndex == 0)
+            {
+                double val = 69990;
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                double val = 84990;
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                double val = 54990;
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                double val = 62990;
+            }
+            else if (listBox1.SelectedIndex == 4)
+            {
+                double val = 39990;
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            if (checkedListBox1.SelectedIndex == 0)
+            {
+                double addVal = 2590;
+            }
+            else if (checkedListBox1.SelectedIndex == 0 && checkedListBox1.SelectedIndex == 1)
+            {
+                double addVal = 1990 + 2590;
+            }
+            else if (checkedListBox1.SelectedIndex == 1 && checkedListBox1.SelectedIndex == 2)
+            {
+                double addVal = 1990 + 2490;
+            }
+            else
+            {
+                double addVal = 1990 + 2490 + 2590;
+            }
+        }
+
+        private void compute_Click(object sender, EventArgs e)
+        {
+            double result = addVal + val;
+            MessageBox.Show($" ",{ result},"Greetings",MessageBoxButtons.OK,MessageBoxButtons.Information);
+        }
+    }
+}
